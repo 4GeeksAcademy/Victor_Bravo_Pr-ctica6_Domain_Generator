be0ae03ccbd8584e05e6be33928d7dd2b693f4ce
Víctor Bravo <vbravo.quintana@gmail.com>
2024-07-03 20:26:55 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your"];
+  let adj = ["greatest", "biggest", "smartest", "best", "almighty"];
+  let noun = ["car", "bikes", "hats", "shoes", "shirt"];
+  let domain = [".com", ".org", ".io", ".us", ".net"];
+
+  const domainGenerator = () => {
+    let numeroAleatorioDecimal = Math.random();
+    let indexPronoun = Math.floor(numeroAleatorioDecimal * pronoun.length);
+    let indexAdj = Math.floor(numeroAleatorioDecimal * adj.length);
+    let indexNoun = Math.floor(numeroAleatorioDecimal * noun.length);
+    let indexDomain = Math.floor(numeroAleatorioDecimal * domain.length);
+
+    let wordPronoun = pronoun[indexPronoun];
+    let wordAdj = adj[indexAdj];
+    let wordNoun = noun[indexNoun];
+    let wordDomain = domain[indexDomain];
+
+    let generator = "www." + wordPronoun + wordAdj + wordNoun + wordDomain;
+
+    return generator;
+  };
+
+  console.log(domainGenerator());
+
+  document.querySelector("#generator").innerHTML = domainGenerator();
 };
